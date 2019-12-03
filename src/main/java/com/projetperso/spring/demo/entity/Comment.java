9e08825518ee408c0e9f533ab5ffa502b8d12739
juliagayral <ju.gayral@gmail.com>
2019-12-03 11:31:23 +0100

    creation comment entity

---
+++ src/main/java/com/projetperso/spring/demo/entity/Comment.java
@@ -0,0 +1,71 @@
+package com.projetperso.spring.demo.entity;
+
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.Date;
+
+@EntityScan
+@Table(name = "users")
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String content;
+    private Date date;
+    private Boolean like;
+
+    public Comment(String content, Date date, Boolean like) {
+        this.content = content;
+        this.date = date;
+        this.like = like;
+    }
+
+    public Comment(){}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Boolean getLike() {
+        return like;
+    }
+
+    public void setLike(Boolean like) {
+        this.like = like;
+    }
+
+    @Override
+    public String toString() {
+        return "Comment{" +
+                "id=" + id +
+                ", content='" + content + '\'' +
+                ", date=" + date +
+                ", like=" + like +
+                '}';
+    }
+}
---
+++ src/main/java/com/projetperso/spring/demo/entity/User.java
@@ -0,0 +1,15 @@
+package com.projetperso.spring.demo.entity;
+
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+import javax.persistence.Table;
+
+@EntityScan
+@Table(name = "users")
+public class User {
+
+
+
+
+
+}
